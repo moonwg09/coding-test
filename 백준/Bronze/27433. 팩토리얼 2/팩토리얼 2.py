8b9cd0e568ab3c47ece1c3c3546e6d2ed53be6b2
moonwg09 <105470710+moonwg09@users.noreply.github.com>
2026-03-17 11:22:39 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -1,7 +1,9 @@
-num = int(input())
-total = 1
+def fact_r(N):
+    # base condition
+    if N <= 1: return 1
 
-for i in range(1, num):
-    total += (total * i)
-
-print(total)
+    # 재귀적인 분해, 조합
+    return N * fact_r(N-1)
+    
+N = int(input())
+print(fact_r(N))
