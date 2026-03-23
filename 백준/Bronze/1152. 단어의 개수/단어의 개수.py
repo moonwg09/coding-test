927b6c29f7907221025d2834bf43f8da856aabc3
moonwg09 <105470710+moonwg09@users.noreply.github.com>
2026-03-24 08:56:47 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 39824 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -2,3 +2,18 @@ words = input().split()
 
 # 리스트 안에 단어가 몇 개 들어있는지 길이(len)를 구해서 출력합니다.
 print(len(words))
+#if len(sentence) == 0:
+ #   print(0)
+    
+# 2. 문장에 글자가 하나라도 있다면 (정상적인 경우)
+#else:
+    # 단어는 최소 1개부터 시작하므로 초기값을 1로 둡니다.
+#    count = 1
+    
+    # 문장의 글자를 하나씩 확인합니다.
+#    for char in sentence:
+        # 글자가 공백(' ')일 때마다 단어의 개수를 1씩 늘려줍니다.
+ #       if char == ' ':
+  #          count += 1
+            
+    #print(count)
