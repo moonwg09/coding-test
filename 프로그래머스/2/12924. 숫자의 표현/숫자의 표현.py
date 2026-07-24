d4cd0ddca9fc8c6416c289cb8a8fb657fa311e09
moonwg09 <105470710+moonwg09@users.noreply.github.com>
2026-07-24 12:41:18 +0900

    Delete 프로그래머스/2 directory

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = 0
-    
-    # 1부터 n까지의 자연수 중 홀수인 약수의 개수를 구합니다.
-    for i in range(1, n + 1):
-        if n % i == 0 and i % 2 != 0:
-            answer += 1
-            
-    return answer
